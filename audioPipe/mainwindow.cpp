cb0dba301c8367ef30ce0dcbcecf6f92b6a81e78
Kevin Briggs <krb686@gmail.com>
2013-06-18 14:38:58 -0400

    -Added menus, exit action, file open action, and tab widget

--- audioPipe/mainwindow.cpp
+++ audioPipe/mainwindow.cpp
@@ -1,14 +1,57 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+#include <QFileDialog>
 
+void fileOpen()
+{
+
+    QString fileName = QFileDialog::getOpenFileName(0);
+
+    std::string text = fileName.toLocal8Bit().constData();
+    std::cout << text;
+    /*
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::critical(this, tr("Error"), tr("Could not open file"));
+            return;
+        }
+        QTextStream in(&file);
+        ui->textEdit->setText(in.readAll());
+        file.close();
+    }
+
+
+    //cout << "Hello!";
+    */
+}
+
+//Constructor
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QObject::connect(ui->actionOpen, &QAction::triggered, fileOpen);
 }
 
+//Destructor
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- audioPipe/mainwindow.h
+++ audioPipe/mainwindow.h
@@ -1,6 +1,5 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
 
 namespace Ui {
@@ -10,13 +9,19 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
     
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    
+
+public slots:
+    //
+
 private:
     Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
+
+
