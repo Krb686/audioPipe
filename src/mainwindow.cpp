a9fb95ada80b014d2627f9d9ef1d8f3d202c6467
Kevin Briggs <krb686@gmail.com>
2013-06-20 14:07:53 -0400

    -Changed QTableWidget to QTextEdit for much faster performance

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -11,6 +11,8 @@
 #include <fcntl.h>
 #include <io.h>
 #include <stdio.h>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -32,47 +34,56 @@ MainWindow::~MainWindow()
 void MainWindow::configure(){
 
     QObject::connect(ui->actionOpen, &QAction::triggered, this, &MainWindow::openFile);
-    ui->tableWidget->setColumnCount(16);
-    ui->tableWidget->setRowCount(1);
 }
 
 void MainWindow::openFile()
 {
     size_t size;
     char *memblock;
+    char *hexBuffer;
 
     QString fileName = QFileDialog::getOpenFileName(0);
-    string text = fileName.toStdString();
+    string filePath = fileName.toStdString();
 
-    ifstream file (text, ios::in|ios::binary|ios::ate);
+    ifstream file (filePath, ios::in|ios::binary|ios::ate);
     if (file.is_open())
     {
         size = file.tellg();
         memblock = new char [size+1];
+        hexBuffer = new char [size+1];
         file.seekg(0, ios::beg);
         file.read(memblock, size);
         file.close();
     }
+    qDebug() << "done reading";
 
-    uint i, rowCount, colCount = 0;
+    uint i = 0;
 
+    QByteArray array = QByteArray(memblock, size).toHex().toUpper();
+    QString s;
+
+    int count1 = 0;
+    int count2 = 0;
     for(i=0;i<size;i++)
     {
-        QTableWidgetItem *item = new QTableWidgetItem(QString::number(memblock[i], 16).toUpper());
-        ui->tableWidget->setItem(rowCount, colCount, item);
-        colCount++;
-
-        if (colCount == 16)
+        count1++;
+        count2++;
+        s.append(array.at(i));
+        if (count1 == 2)
         {
-            rowCount++;
-            colCount-=16;
-            ui->tableWidget->setRowCount(rowCount+1);
+            s.append('\t');
+            count1-= 2;
         }
-
+        if (count2 == 32)
+        {
+            s.append("\n\n");
+            count2-= 32;
+        }
     }
 
-
     delete[] memblock;
+    qDebug() << "done making string";
+    ui->textEdit->setText(s);
 }
 
 
